f4aed3829d4a82527bca5cf4f1645059b3f6d761
Ahmad Raihan Asyam <araihanassyam@gmail.com>
2024-06-19 10:34:30 +0800

    revert

--- pages/menu/menu_nasi.php
+++ pages/menu/menu_nasi.php
@@ -64,6 +64,12 @@ echo "<script>var namaFilePHP = '{$nama_file_php}';</script>";
                     <li class="nav-item">
                         <a class="nav-link" href="" data-bs-toggle="modal" data-bs-target="#myModal">Reservasi</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../event/event.php">Acara</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../event/event.php#wedding">Pernikahan</a>
+                    </li>
                 </ul>
             </div>
         </div>
